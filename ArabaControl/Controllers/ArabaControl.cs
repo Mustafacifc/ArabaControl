80b70ed63dcc284535d07206c4680931c74cf63e
mustafa471 <mustafa471@BPC12>
2024-02-16 09:32:04 +0300

    Proje dosyası ekle.

---
+++ ArabaControl/Controllers/ArabaControl.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArabaControl.Controllers
+{
+    public class ArabaControl : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(new
+            {
+                Id = 1,
+                Seviye = "ChpyerTruck",
+                MarkaAdi = "Tesla"
+            });
+        }
+    }
+}
